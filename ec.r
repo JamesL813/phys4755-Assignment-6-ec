8d85f059ba1856a861857d8ae4f390d8630d57e3
James Lawton <JCL813@proton.me>
2022-12-13 18:00:54 -0500

    shape

---
+++ ec.r
@@ -0,0 +1,13 @@
+library("scatterplot3d") # load
+
+data <- read.csv("xyz.data", header = TRUE, sep = " ")
+png(filename = "shape.png", width = 8, height = 6, units = "in", res = 300)
+scatterplot3d(data$x, data$y, data$z,
+    type = "p",
+    pch = 16,
+    highlight.3d = TRUE,
+    box = TRUE,
+    main="shape",
+    xlab = "x",
+    ylab = "y",
+    zlab = "z")
